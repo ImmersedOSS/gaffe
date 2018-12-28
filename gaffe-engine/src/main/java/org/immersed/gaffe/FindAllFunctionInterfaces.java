79beae7dfd7eabe709a7b174b0ed6c466557fe7a
Daniel Avila <masterav10@gmail.com>
2018-12-27 19:13:49 -0500

    Problem: Need to see method and params for function interfaces.
    Solution: Update test class to print this information.

--- gaffe-engine/src/main/java/org/immersed/gaffe/FindAllFunctionInterfaces.java
+++ gaffe-engine/src/main/java/org/immersed/gaffe/FindAllFunctionInterfaces.java
@@ -3,7 +3,9 @@ package org.immersed.gaffe;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import io.github.classgraph.MethodParameterInfo;
 
 public class FindAllFunctionInterfaces
 {
@@ -12,13 +14,27 @@ public class FindAllFunctionInterfaces
         PrintStream stream = new PrintStream(new File("out.txt"));
         System.setErr(stream);
 
-        AtomicInteger count = new AtomicInteger();
         FunctionalInterfaceSet.jdk()
                               .toList()
                               .stream()
-                              .map(spec -> spec.superClassInfo())
-                              .map(ci -> ci.getName())
-                              .map(name -> count.incrementAndGet() + " " + name)
-                              .forEach(System.out::println);
+                              .forEach(s ->
+                              {
+                                  String name = s.superClassInfo()
+                                                 .getName();
+                                  String m = s.superMethodInfo()
+                                              .getName();
+
+                                  AtomicReference<String> ref = new AtomicReference<>("");
+                                  StringBuilder builder = new StringBuilder("(");
+                                  for (MethodParameterInfo param : s.superMethodInfo()
+                                                                    .getParameterInfo())
+                                  {
+                                      builder.append(ref.getAndSet(", "));
+                                      builder.append(param.getTypeSignatureOrTypeDescriptor());
+                                  }
+                                  builder.append(")");
+
+                                  System.out.println(String.format("%s.%s%s", name, m, builder.toString()));
+                              });
     }
 }
